1521fa06470a71c918782df9b63fb6fac949a639
Yang Zhang <yza457@sfu.ca>
2020-02-08 14:11:06 -0800

    added create_table

--- data.py
+++ data.py
@@ -6,14 +6,22 @@ import json
 def create_db():
     try:
         conn = sqlite3.connect(os.getcwd() + "/wardrobe.db")
+        create_table = """  CREATE TABLE IF NOT EXISTS wardrobe (
+                                id integer PRIMARY KEY,
+                                type integer NOT NULL,
+                                name varchar(20)
+                            ); """
+        conn.execute(create_table)
     except Error as e:
         print(e)
     finally:
         if conn:
             conn.close()
 
+
+
 if __name__ == '__main__':
-    # create_db()
-    with open(os.getcwd() + "/test.json", 'r') as f:
-        test = json.load(f)
-        print(test[0]['Name'])
+    create_db()
+    # with open(os.getcwd() + "/test.json", 'r') as f:
+    #     test = json.load(f)
+    #     print(test[-1]['Kernel'])
