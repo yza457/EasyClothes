dd7794c6768dec74a4e35db51c866612c4cc20b4
WENXU-codinglife <wenxuw@sfu.ca>
2020-02-08 15:29:30 -0800

    update

--- data.py
+++ data.py
@@ -6,11 +6,11 @@ import json
 def create_db():
     try:
         conn = sqlite3.connect(os.getcwd() + "/wardrobe.db")
-        create_table = """  CREATE TABLE IF NOT EXISTS wardrobe (
-                                id integer PRIMARY KEY,
-                                type integer NOT NULL,
-                                name varchar(20)
-                            ); """
+        create_table = """CREATE TABLE wardrobe_table ( 
+            id integer PRIMARY KEY,
+            type integer NOT NULL,
+            name varchar(20),
+            location varchar(50))"""
         conn.execute(create_table)
     except Error as e:
         print(e)
@@ -18,10 +18,31 @@ def create_db():
         if conn:
             conn.close()
 
+def store_insert(items):
+    db = sqlite3.connect(':wardrobe:')    
+    cursor = db.cursor()
+    count = 0
+    index = 0
+    for keys in items:
+        cursor.execute('SELECT COUNT(*) FROM wardrobe_table;', (count))
+        clothesNo = count+1
+        nickname = items(index,['name'])
+        clothestype = items(index,['type'])
+        location = items(index,['location'])
+        cursor.execute('''INSERT INTO wardrobe_table(id, type, name, location)
+                        VALUES(?,?,?,?)''', (clothesNo,nickname, clothestype, location))
+    db.commit()
 
 
 if __name__ == '__main__':
-    create_db()
+    if not os.path.exists("wardrobe.db"):
+        create_db()
+    items = {
+        "name": "Ford",
+        "type": "Mustang",
+        "location": "closet"
+    }
+    store_insert(items)
     # with open(os.getcwd() + "/test.json", 'r') as f:
     #     test = json.load(f)
     #     print(test[-1]['Kernel'])
