0d89b5dee84692f0d2189271c503f5d27af9cd65
eileencai <yuner_cai@sfu.ca>
2020-02-13 16:51:13 -0800

    update front

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+import data
+
+app = Flask(__name__, static_folder='', template_folder='')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/server', methods=['POST'])
+def server():
+    # print(request.form)
+    # name = request.form['name']
+    # print("1")
+    # print("2")
+    # print(request.form)
+    # n = request.form['name']
+    # t = request.form['type']
+    # loc = request.form['location'])
+    data.store_insert(request.form)
+    # type = request.form['form']
+    # location = request.form['location']
+    # submit = request.form['submit']
+    return "ok"
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
