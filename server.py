899e1c0937a5ec9f2ba1d6499e1d96c94fbc8df6
Yang Zhang <yza457@sfu.ca>
2020-03-05 14:20:28 -0800

    temp working of saving images

--- server.py
+++ server.py
@@ -1,27 +1,33 @@
 from flask import Flask, render_template, request, jsonify
 import data
 
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__, static_folder='', template_folder='')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 # used for store clothes data into database
 @app.route('/server', methods=['POST'])
 def server():
-    # print(request.form)
-    # name = request.form['name']
-    # print("1")
-    # print("2")
+    # first get text info
     print(request.form)
-    # n = request.form['name']
-    # t = request.form['type']
-    # loc = request.form['location'])
     data.store_insert(request.form)
-    # type = request.form['form']
-    # location = request.form['location']
-    # submit = request.form['submit']
+    # then get the image
+    if 'file' in request.files:
+        file = request.files['file']
+        if file.filename == '':
+            return "no file name"
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return "ok"
 
 # used for store clothes data into database
